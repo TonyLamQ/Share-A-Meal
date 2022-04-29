e621754a05e167cded1a11a9a647225b69160070
Tony <tnd.lamquang@student.avans.nl>
2022-04-29 12:34:28 +0200

    created mysql

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+require('dotenv').config();
+
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 const BodyParser = require('body-parser');
 app.use(BodyParser.json());
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,5 @@
-let database = [];
-let id = 0;
+const dbConn = require('../../database/dbConnection')
+const database = require('../../database/inmemdb')
 const assert = require('assert');
 
 let controller = {
@@ -50,10 +50,30 @@ let controller = {
     },
 
     getAllUsers:(req,res)=>{  
-        res.status(200).json({
+      dbConn.getConnection(function(err, connection) {
+        if (err) throw err; // not connected!
+       
+        // Use the connection
+        connection.query('SELECT * FROM user', function (error, results, fields) {
+          // When done with the connection, release it.
+          connection.release();
+       
+          // Handle error after the release.
+          if (error) throw error;
+       
+          // Don't use the connection here, it has been returned to the pool.
+          console.log("Results = ", results.length);
+          res.status(200).json({
             status: 200,
-            result: database,
-      })
+            results: results
+          })
+    
+          // pool.end((err) => {
+          //   console.log("pool was closed.");
+          //   });
+          
+        });
+      });
     },
 
     getUserProfile:(req,res)=>{  
