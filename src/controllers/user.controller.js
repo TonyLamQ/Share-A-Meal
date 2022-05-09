c0cf4bb71f71d91374288ba67445eb1e7ee0cb27
Tony <tnd.lamquang@student.avans.nl>
2022-05-09 19:59:03 +0200

    Converted localDb to mysql db

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -23,30 +23,49 @@ let controller = {
                 next(error);
             }
         },
-    addUser:(req, res) => {
-        let email = req.body.emailAddress;
-        if (database.filter((item) => item.emailAddress == email).length > 0) {
-            res.status(400).json({
-                Status: 400,
-                Message: `This email is already taken.`
+    addUser:(req, res, next) => {
+
+      dbConn.getConnection(function(err, connection) {
+        if (err) throw err; // not connected!
+       
+        // Use the connection
+        const firstName = req.body.firstName;
+        const lastname = req.body.lastName;
+        const emailAddress = req.body.emailAddress;
+        const password = req.body.password;
+        const street = req.body.street;
+        const city = req.body.city;
+        connection.query(`INSERT INTO user (firstName, lastName, emailAdress, password, street, city) VALUES ('${firstName}','${lastname}','${emailAddress}','${password}','${street}','${city}')`, function (error, results, fields) {
+          // When done with the connection, release it.
+          connection.release();
+         // Handle error after the release.
+          if (error) {
+            const error = {
+              status: 404,
+              results: `The email: '${emailAddress}' already exists`,           
+           }
+           next(error);
+          } else {
+       
+          // Don't use the connection here, it has been returned to the pool.
+          console.log("Results = ", results.affectedRows);
+
+          if (results.affectedRows == 1){
+            res.status(200).json({
+              status: 200,
+              results: `User has been created.`,
             })
-        }
-        else {
-         let user = req.body;
-          console.log(user);
-           id++
-           user = {
-            id,
-            ...user,
-           };
-    
-          database.push(user);
-          console.log(`User has been added to the database.`);
-          res.status(201).json({
-            status: 201,
-            result: database,
-          });
-        }
+          } else {                   
+            const error = {
+                status: 404,
+                results: `User could not be created.`,           
+          }
+          next(error);
+        }}
+          
+          
+        });
+      });
     },
 
     getAllUsers:(req,res)=>{  
@@ -84,91 +103,119 @@ let controller = {
     },
 
     getUserById:(req,res, next)=>{
+      dbConn.getConnection(function(err, connection) {
+        if (err) throw err; // not connected!
+       
         const userId = Number(req.params.userId)
-        let user = database.filter((item) => item.id == userId);
-        if (user.length > 0) {
-          console.log(user)
-          res.status(200).json({
-            status: 200,
-            result: user,
-          })
-        }
-        else {
-            const error = {
-                status: 404,
-                result: `User with ID ${userId} not found.`,
-            }
-            next(error);
-        }
-    },
-
-    deleteUser:(req,res)=>{
-        const userId = Number(req.params.userId)
-        //check if id is in database
-        let user = database.filter((item) => item.id == userId);
-        if (user.length > 0) {
-      
-          //check the index of userId
-          var index = database.map(x => {
-            return x.id;
-          }).indexOf(userId);
-      
-          //Remove the id from array
-          console.log("INDEX OF "+index)
-          database.splice(index, 1);
-          console.log(database);
-      
-          res.status(200).json({
-            status: 200,
-            result: `User with ID ${userId} has been deleted.`,
-          })
-        } else {
-          res.status(404).json({
-            status: 404,
-            result: `User with ID ${userId} not found.`,
-          });
-        }
-    },
-
-    updateUser:(req,res)=>{
-        const id = Number(req.params.userId)
-        let email = req.body.emailAddress;
-        let user = database.filter((item) => item.id == id);
-      
-        if (user.length > 0) {
-          if (database.filter((item) => item.emailAddress == email).length > 0) {
-              res.status(400).json({
-                  Status: 400,
-                  Message: `This email is already taken.`
-              })
-          } else {
-            var index = database.map(x => {
-              return x.id;
-            }).indexOf(id);
-        
-            console.log("INDEX OF "+index)
-            database.splice(index, 1);
-            console.log(database);
-      
-            let newUser = req.body;
-            console.log(newUser);
-            newUser = {
-              id,
-              ...newUser,
-           };
-            database.push(newUser);
-            console.log(database);
+        // Use the connection
+        connection.query(`SELECT * FROM user WHERE id = ${userId}`, function (error, results, fields) {
+          // When done with the connection, release it.
+          connection.release();
+       
+          // Handle error after the release.
+          if (error) throw error;
+       
+          // Don't use the connection here, it has been returned to the pool.
+          console.log("Results = ", results.length);
+          if (results.length == 1){
             res.status(200).json({
               status: 200,
-              result: `User with ID ${id} has been updated.`,
+              results: results
             })
+          } else {                   
+            const error = {
+                status: 404,
+                results: `User with ID ${userId} not found.`,           
           }
-        } else {
-          res.status(404).json({
-            status: 404,
-            result: `User with ID ${id} not found.`,
-          });
+          next(error);
         }
+
+    
+          // pool.end((err) => {
+          //   console.log("pool was closed.");
+          //   });
+          
+        });
+      });
+    },
+
+    deleteUser:(req,res, next)=>{
+       dbConn.getConnection(function(err, connection) {
+        if (err) throw err; // not connected!
+       
+        // Use the connection
+        const userId = Number(req.params.userId);
+          
+        connection.query(`DELETE FROM user WHERE id = ${userId}`, function (error, results, fields) {
+          // When done with the connection, release it.
+          connection.release();
+       
+          // Handle error after the release.
+          if (error) throw error;
+       
+          // Don't use the connection here, it has been returned to the pool.
+          console.log("Results = ", results.affectedRows);
+
+          if (results.affectedRows == 1){
+            res.status(200).json({
+              status: 200,
+              results: `User with ID ${userId} has been deleted.`,
+            })
+          } else {                   
+            const error = {
+                status: 404,
+                results: `User with ID ${userId} not found.`,           
+          }
+          next(error);
+        }
+          
+        });
+      });
+    },
+
+    updateUser:(req,res, next)=>{
+      dbConn.getConnection(function(err, connection) {
+        if (err) throw err; // not connected!
+       
+        // Use the connection
+        const userId = Number(req.params.userId);
+        const firstName = req.body.firstName;
+        const lastname = req.body.lastName;
+        const emailAddress = req.body.emailAddress;
+        const password = req.body.password;
+        const street = req.body.street;
+        const city = req.body.city;
+        connection.query(`UPDATE user SET firstName= '${firstName}', lastName= '${lastname}', emailAdress= '${emailAddress}', password= '${password}', street= '${street}', city= '${city}' WHERE id = '${userId}'`, function (error, results, fields) {
+          // When done with the connection, release it.
+          connection.release();
+       
+          // Handle error after the release.
+          if (error) {
+            const error = {
+              status: 404,
+              results: `The email: '${emailAddress}' already exists`,           
+           }
+           next(error);
+          } else {
+       
+          // Don't use the connection here, it has been returned to the pool.
+          console.log("Results = ", results.affectedRows);
+
+          if (results.affectedRows == 1){
+            res.status(200).json({
+              status: 200,
+              results: `User with ID ${userId} has been updated.`,
+            })
+          } else {                   
+            const error = {
+                status: 404,
+                results: `User with ID ${userId} not found.`,           
+          }
+          next(error);
+        }}
+          
+        });
+      });
     },
 
 };
