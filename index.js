8b9bed078976f24994f769e2e19c14caf46c97b5
TonyLamQ <tnd.lamquang@student.avans.nl>
2022-05-19 15:12:12 +0200

    implemented mealController&Route

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
-const express = require('express')
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT;
 
-const BodyParser = require('body-parser');
+const BodyParser = require("body-parser");
 app.use(BodyParser.json());
 
-const router = require('./src/routes/user.routes');
+const userRouter = require("./src/routes/user.routes");
+const mealRouter = require("./src/routes/meal.routes");
 
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   const method = req.method;
   console.log(`Method ${method} is called.`);
   next();
 });
 
-app.use(router);
+app.use(userRouter);
+app.use(mealRouter);
+
 app.all("*", (req, res) => {
   res.status(401).json({
     status: 401,
     results: "End-point not found.",
-  })
-})
+  });
+});
 
 //error handler
 app.use((err, req, res, next) => {
   res.status(err.status).json(err);
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}.`)
+  console.log(`Example app listening on port ${port}.`);
 });
 
 module.exports = app;
